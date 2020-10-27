7b8d30e85727e835057ce658837ff2dd64ea78f9
Jatinrajpal <jatinrajpal57@gmail.com>
2020-10-28 03:51:09 +0530

    created grid with n rows and m cols

---
+++ src/components/Node.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Node.css"
+const Node = ({isStart,isEnd}) => {
+    const classes=isStart?"node-start":isEnd?"node-End":"";
+return <div className={'node $(classes)'}> </div>;
+};
+export default Node;
--- src/components/Pathfind.js
+++ src/components/Pathfind.js
@@ -1,18 +1,28 @@
 import React,{useState,useEffect} from "react";
 import App from "../App";
-const cols=5;
-const rows=5;
+import Node from "./Node";
+import "./Pathfind.css";
+const cols=15;
+const rows=25;
+const NODE_START_ROW=0;
+const NODE_START_COL=0;
+const NODE_END_ROW=rows-1;
+const NODE_END_COL=cols-1;
 const Pathfind=()=>{
-    const{Grid,setGrid}=useState([]);
-    const initiaize=()=>{
+    const [Grid,setGrid] = useState([]);
+    useEffect( () => {
+        initalizeGrid();
+    }, []);
+    const initalizeGrid=()=>{
         const grid=new Array(cols);
         for(let i=0;i<cols;i++)
         {
             grid[i]=new Array(rows);
         }
-        craeteSpot(grid);
+        createSpot(grid);
+        setGrid(grid);
     };
-    const craeteSpot=(grid)=>{
+    const createSpot= (grid) => {
         for(let i=0;i<cols;i++)
         {
             for(let j=0;j<rows;j++)
@@ -25,14 +35,33 @@ const Pathfind=()=>{
     {
         this.x=i;
         this.y=j;
+        this.isStart=this.x==NODE_START_ROW && this.y==NODE_START_COL;
+        this.isEnd=this.x==NODE_END_ROW && this.y==NODE_END_COL;
         this.g=0;
         this.f=0;
         this.h=0;
     }
-    return (
+    //GRID WITH NODE
+    const gridwithNode=(
         <div>
-            <h1>Pathfind Component</h1>
+        {Grid.map((row,rowIndex) => {
+            return (
+                <div key={rowIndex} className="rowWrapper">
+                    {row.map((col,colIndex)=>{
+                        const {isStart,isEnd}=col;
+                        return <Node key={colIndex} isStart={isStart} isEnd={isEnd}></Node>;
+                    })}
+                </div>
+            )
+        })}
             </div>
+    )
+    // console.log(Grid);
+    return (
+        <div className="Wrapper">
+            <h1>Pathfind Component</h1>
+            {gridwithNode}
+        </div>
     );
 };
 export default Pathfind;
